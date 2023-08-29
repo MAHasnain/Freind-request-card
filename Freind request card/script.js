54d0ca29030e754ffcbc0b9c40dacc70f90a5a76
M A Hasnain <attaulhasnainmaqsoodi@gmail.com>
2023-08-29 10:21:00 +0500

    first task complete

--- Freind request card/script.js
+++ Freind request card/script.js
@@ -1,17 +1,27 @@
 let istatus = document.querySelector("h4")
 
-let addFreind = document.querySelector("#add")
-let removeFreind = document.querySelector("#remove")
+let btn = document.querySelector("#add")
 
-addFreind.addEventListener("click", function(){
-    istatus.innerHTML = "Friend"
-    istatus.style.color = "green"
+let flag = 0
+
+btn.addEventListener("click", function(){
+    if (flag == 0) {
+        istatus.innerHTML = "Friend"
+        istatus.style.color = "green"
+        btn.style.backgroundColor = "Red"
+        btn.innerHTML = "Remove"
+        flag = 1
+    }
+    else {
+        istatus.innerHTML = "President"
+        istatus.style.color = "black"
+        btn.innerHTML = "Add friend"
+        btn.style.backgroundColor = "Green"
+        flag = 0
+    }
+
 })
 
-removeFreind.addEventListener("click", function() {
-    istatus.innerHTML = "President"
-    istatus.style.color = "black"
-})
 
 
 
